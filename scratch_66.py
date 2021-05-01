acdd715ba939e19a308cc207290f1d28c56fb5c0
docmab23 <31487695+docmab23@users.noreply.github.com>
2021-05-01 08:22:54 +0530

    Add files via upload

---
+++ scratch_66.py
@@ -0,0 +1,68 @@
+import os
+from flask import Flask, request, abort, jsonify, send_from_directory
+from werkzeug.utils import secure_filename
+from flask import Flask, redirect, url_for, request, render_template
+import logging
+#from gevent.pywsgi import WSGIServer
+from keras.models import load_model
+import numpy as np
+from sklearn.metrics import mean_absolute_error as mae
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+import tensorflow as tf
+arrs = []
+MODEL_PATH = "C:/Users/Administrator/.PyCharmCE2019.2/config/scratches/Epiassist_model2.h5"
+Threshold =   1.271  # Threshold set after training the model (set at 3 Std deviations from the mean reconstruction error)
+m = load_model(MODEL_PATH)
+
+def flatten(tensor):
+    flattened_X = np.empty((tensor.shape[0], tensor.shape[2]))  # sample x features array.
+    for i in range(tensor.shape[0]):
+        flattened_X[i] = tensor[i, (tensor.shape[1] - 1), :]
+    return (flattened_X)
+
+api = Flask(__name__)
+
+@api.route("/",methods=["GET"])
+def start():
+    #try:
+	  return ("Epiassist_API is working" , 200)
+    #except:
+     # return (404)
+
+@api.route("/gdap",methods=["POST"])
+
+def gdap():
+    #arrs =[]
+    oxy = float(request.args.get('oxy'))
+    temp= float(request.args.get('temp'))
+    emg = float(request.args.get('emg'))
+    hrt = float(request.args.get('hrt'))
+    acc = float(request.args.get('acc'))
+    arr = [oxy , temp , emg , hrt , acc]
+    #pred = m.predict(arr)
+    arrs.append(arr)
+    print(api.logger.info(arr))
+    #return jsonify(arrs)
+    if len(arrs)>= 10:
+          X =np.array(arrs[len(arrs)-10: len(arrs)])
+          X = StandardScaler().fit_transform(X)
+          X =np.reshape(X ,(1,10,5))
+          pred= m.predict(X)
+          err = tf.keras.losses.mae(flatten(X), flatten(pred))
+          #print(np.shape(X))
+          if err>=Threshold:
+              return ("Seizure is near!")
+              #print(err)
+          else:
+             return("Normal")
+
+    return "Get more data"
+
+if __name__ == '__main__':
+    api.run(debug=True)
+
+
+
+
+
+
