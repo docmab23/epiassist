9a62fd2ad8adc5c3396729d1535dff7fc1ed6b0d
docmab23 <31487695+docmab23@users.noreply.github.com>
2021-05-03 00:09:48 +0530

    Update Api.py

--- Api.py
+++ Api.py
@@ -10,7 +10,7 @@ from sklearn.metrics import mean_absolute_error as mae
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 import tensorflow as tf
 arrs = []
-MODEL_PATH = "C:/Users/Administrator/.PyCharmCE2019.2/config/scratches/Epiassist_model2.h5"
+MODEL_PATH = "Epiassist_model2.h5"
 Threshold =   1.271  # Threshold set after training the model (set at 3 Std deviations from the mean reconstruction error)
 m = load_model(MODEL_PATH)
 
