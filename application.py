d3b715a0a5da6afe24b7c7add0e618e642618476
docmab23 <31487695+docmab23@users.noreply.github.com>
2021-05-15 19:13:21 +0530

    Update application.py

--- application.py
+++ application.py
@@ -4,82 +4,89 @@ from werkzeug.utils import secure_filename
 from flask import Flask, redirect, url_for, request, render_template
 from flask_ngrok import run_with_ngrok
 import logging
-#from gevent.pywsgi import WSGIServer
+# from gevent.pywsgi import WSGIServer
 from keras.models import load_model
 import numpy as np
 from sklearn.metrics import mean_absolute_error as mae
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 import tensorflow as tf
 import requests
+
 url = "https://www.fast2sms.com/dev/bulkV2"
 
-querystring = {"authorization":"paiXGTsvwx2UYNuFJlZVocjmKQBLrgd4IeqMkAb57z9OSRn0hywWgIxHZpbnY2OasUKEy4ACd3QTekvP","message":"Seizure is near , please help the patient","language":"english","route":"q","numbers":"9888953231,9772900422"}
+querystring = {"authorization": "paiXGTsvwx2UYNuFJlZVocjmKQBLrgd4IeqMkAb57z9OSRn0hywWgIxHZpbnY2OasUKEy4ACd3QTekvP",
+               "message": "Seizure is near , please help the patient", "language": "english", "route": "q",
+               "numbers": "9888953231,9772900422"}
 
 headers = {
     'cache-control': "no-cache"
 }
 
-
-
-
-MODEL_PATH = "./model_files/Epiassist_model3.h5"
-Threshold =   1.21249 # Threshold set after training the model (set at 3 Std deviations from the mean reconstruction error)
+MODEL_PATH = "C:/Users/Administrator/.PyCharmCE2019.2/config/scratches/Epi_5.h5"
+Threshold = 7423.16  # Threshold set after training the model (set at 3 Std deviations from the mean reconstruction error)
 m = tf.keras.models.load_model(MODEL_PATH)
+
+
 def flatten(tensor):
     flattened_X = np.empty((tensor.shape[0], tensor.shape[2]))  # sample x features array.
     for i in range(tensor.shape[0]):
         flattened_X[i] = tensor[i, (tensor.shape[1] - 1), :]
     return (flattened_X)
 
-application = Flask(__name__)
-run_with_ngrok(application)
-@application.route("/",methods=["GET", "POST"])
+
+application2 = Flask(__name__)
+run_with_ngrok(application2)
+
+
+@application2.route("/", methods=["GET", "POST"])
 def start():
-    #try:
-	  return ("Epiassist_API is working" , 200)
-    #except:
-     # return (404)
+    # try:
+    return ("Epiassist_API is working", 200)
+
+
+# except:
+# return (404)
 arrs = []
-@application.route("/gdap",methods=["POST"])
 
+
+@application2.route("/gdap", methods=["POST"])
 def gdap():
- 
-    oxy = (request.args.get('oxy'))
-    temp= (request.args.get('temp'))
-    #emg = float(request.args.get('emg'))
-    hrt = (request.args.get('hrt'))
-    acc = (request.args.get('acc'))
-    arr = [oxy , temp , hrt , acc]
-    #pred = m.predict(arr)
-    arrs.append(arr)
-    print(application.logger.info(arr))
+    oxy =  request.args.get('oxy')
+    temp = request.args.get('temp')
+    # emg = float(request.args.get('emg'))
+    hrt = request.args.get('hrt')
+    acc = request.args.get('acc')
+    arr = [oxy, temp, hrt, acc]
+    # pred = m.predict(arr)
+    #arrs.append(arr)
+    print(application2.logger.info(arr))
     print(arr)
-    #return jsonify(arrs)
-    if len(arrs)>= 10:
-          X =np.array(arrs[len(arrs)-10: len(arrs)])
-          X = StandardScaler().fit_transform(X)
-          X =np.reshape(X ,(1,10,4))
-          pred= m.predict(X)
-          err = tf.keras.losses.mae(flatten(X), flatten(pred))
-          #print(np.shape(X))
-          if err>=Threshold:
+    X = np.array(arr)
+    X = X.astype(np.float)
 
-              response = requests.request("GET", url, headers=headers, params=querystring)
-              print(response.text)
+    # X = StandardScaler().fit_transform(X)
+    X = np.reshape(X, (1, 1, 4))
+    p = m.predict(X)
+    err = tf.keras.losses.mae(flatten(X), flatten(p))
+    # print(np.shape(X))
+    if err >= Threshold:
 
-              return ("Seizure is near!")
+            response = requests.request("GET", url, headers=headers, params=querystring)
+            print(response.text)
 
-              #print(err)
-          else:
-             return("Normal")
+            return ("Seizure is near!")
 
-    return ("Initializing")
+            # print(err)
+    else:
+            return ("Normal")
+
+
 
 if __name__ == '__main__':
-    application.run()
+    application2.run()
 
 
 
 
 
-
+#C:\Users\Administrator\.PyCharmCE2019.2\config\scratches
